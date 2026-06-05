0a0fdf3729596ac69e19b6c03a4a76af397fbd97
Robert C. Martin <unclebob@cleancoder.com>
2026-06-05 14:21:19 -0500

    Implement Task 3 crooked arrows

---
+++ test/htw/shooting_test.clj
@@ -0,0 +1,55 @@
+(ns htw.shooting-test
+  (:require [clojure.test :refer [deftest is]]
+            [htw.game :as game]))
+
+(defn configured-game
+  [player wumpus options]
+  (merge (game/configured-game player wumpus #{} #{})
+         {:arrows 5}
+         options))
+
+(deftest arrow-hits-wumpus-and-player
+  (let [hit (game/shoot-arrow (configured-game 1 11 {}) [2 10 11])
+        self-hit (game/shoot-arrow (configured-game 1 13 {}) [2 1])]
+    (is (= [2 10 11] (:arrow-visits hit)))
+    (is (= :won (:status hit)))
+    (is (= ["AHA! YOU GOT THE WUMPUS!"] (:messages hit)))
+    (is (= [2 1] (:arrow-visits self-hit)))
+    (is (= :lost (:status self-hit)))
+    (is (= ["OOPS! ARROW GOT YOU!"] (:messages self-hit)))))
+
+(deftest invalid-arrow-segment-deviates
+  (let [state (configured-game 1 13 {:arrow-deviation-room 5
+                                     :wumpus-wake-choice "stay"})
+        result (game/shoot-arrow state [3 4])]
+    (is (= [5 4] (:arrow-visits result)))
+    (is (= 4 (:arrows result)))
+    (is (= :in-progress (:status result)))))
+
+(deftest missed-arrow-wakes-wumpus-and-can-lose-on-exhaustion
+  (let [miss (game/shoot-arrow
+               (configured-game 1 10 {:wumpus-wake-choice "move to 2"})
+               [5])
+        eaten (game/shoot-arrow
+                (configured-game 1 10 {:wumpus-wake-choice "move to 1"})
+                [5])
+        out (game/shoot-arrow
+              (configured-game 1 10 {:arrows 1
+                                     :wumpus-wake-choice "stay"})
+              [5])]
+    (is (= 4 (:arrows miss)))
+    (is (= 2 (:wumpus-room miss)))
+    (is (= :in-progress (:status miss)))
+    (is (= :lost (:status eaten)))
+    (is (= 0 (:arrows out)))
+    (is (= :lost (:status out)))
+    (is (= ["YOU RAN OUT OF ARROWS"] (:messages out)))))
+
+(deftest invalid-shot-is-rejected-without-spending-arrow
+  (let [empty-shot (game/try-shoot-arrow (configured-game 1 10 {}) [])
+        long-shot (game/try-shoot-arrow (configured-game 1 10 {}) [2 10 11 12 13 14])]
+    (is (= "CAN'T SHOOT THERE" (:error empty-shot)))
+    (is (= 5 (:arrows empty-shot)))
+    (is (= :in-progress (:status empty-shot)))
+    (is (= "CAN'T SHOOT THERE" (:error long-shot)))
+    (is (= 5 (:arrows long-shot)))))
--- test/htw/test_runner.clj
+++ test/htw/test_runner.clj
@@ -1,10 +1,12 @@
 (ns htw.test-runner
   (:require [clojure.test :as test]
             [htw.domain-test]
-            [htw.movement-test]))
+            [htw.movement-test]
+            [htw.shooting-test]))
 
 (defn -main [& _]
   (let [{:keys [fail error]} (test/run-tests 'htw.domain-test
-                                             'htw.movement-test)]
+                                             'htw.movement-test
+                                             'htw.shooting-test)]
     (when (pos? (+ fail error))
       (System/exit 1))))
