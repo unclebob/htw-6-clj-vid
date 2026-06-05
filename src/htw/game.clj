41e0485a4017cf7770b4cedc7d4a94c5edc21f26
Robert C. Martin <unclebob@cleancoder.com>
2026-06-05 14:47:42 -0500

    Harden task1 setup mutations

--- src/htw/game.clj
+++ src/htw/game.clj
@@ -26,3 +26,7 @@
     {:wumpus (if (neighbors wumpus-room) 1 0)
      :pit (count (filter neighbors pit-rooms))
      :bat (count (filter neighbors bat-rooms))}))
+
+;; clj-mutate-manifest-begin
+;; {:version 1, :tested-at "2026-06-05T14:45:49.612174-05:00", :module-hash "1143932432", :forms [{:id "form/0/ns", :kind "ns", :line 1, :end-line 3, :hash "-878077022"} {:id "defn-/place-entities", :kind "defn-", :line 5, :end-line 10, :hash "1431118401"} {:id "defn/start-game", :kind "defn", :line 12, :end-line 13, :hash "1737958242"} {:id "defn/reuse-setup", :kind "defn", :line 15, :end-line 16, :hash "-1770652719"} {:id "defn/hazard-rooms", :kind "defn", :line 18, :end-line 19, :hash "-1217263101"} {:id "defn/occupied-rooms", :kind "defn", :line 21, :end-line 22, :hash "751994569"} {:id "defn/adjacent-hazards", :kind "defn", :line 24, :end-line 28, :hash "-882909919"}]}
+;; clj-mutate-manifest-end
